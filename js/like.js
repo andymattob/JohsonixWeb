d08cc4c547756fdcfd01d702268b25708d7afebf
Andreas Johansson <86415353+andymattob@users.noreply.github.com>
2024-01-25 23:04:43 +0100

    test

--- js/like.js
+++ js/like.js
@@ -1,9 +1,20 @@
-//like buttons
-$(".like-area a").click(function() {
-    if ($(this).hasClass("like-active")) {
-     $(this).find('.like-no').html(parseInt($(this).find('.like-no').html(), 10) - 1)
-    } else {
-     $(this).find('.like-no').html(parseInt($(this).find('.like-no').html(), 10) + 1)
-    }
-    $(this).toggleClass('like-active');
-   });
+var btn1 = document.querySelector('#green');
+var btn2 = document.querySelector('#red');
+
+btn1.addEventListener('click', function() {
+  
+    if (btn2.classList.contains('red')) {
+      btn2.classList.remove('red');
+    } 
+  this.classList.toggle('green');
+  
+});
+
+btn2.addEventListener('click', function() {
+  
+    if (btn1.classList.contains('green')) {
+      btn1.classList.remove('green');
+    } 
+  this.classList.toggle('red');
+  
+});
